2ea0cd1db0bcc64a73ce4fcb39cf01f01a94b5e0
Vera Yamatina <runatique@gmail.com>
2024-09-26 01:01:26 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('Curriculum Vitae пока не готов')
+})
+/* Убрать предыдущий код и вставить этот, когла будет готово резюме*/
+button.addEventListener('click', function() {
+	const link = document.createElement('a');
+	link.href = './cv.pdf'; // Укажите путь к вашему файлу
+	link.download = 'cv.pdf'; // Имя файла для скачивания
+	link.click();
+})
